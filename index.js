dcbb5dc678753932dc558ab23e32ad63289339dd
PILIPSCODE <95007762+PILIPSCODE@users.noreply.github.com>
2023-04-05 14:30:56 +0700

    up

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ app.use(cors())
 app.use(cookieParser())
 
 
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,PATCH,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers' ,'Content-Type, Authorization');
+  next()
+})
+
 app.use("/upload",express.static(patho.join(__dirname,"upload")))
 // app.use('/',uploadImageroute)
 app.use('/',signroute)
